cfbc427ef635f20d07e52bc24dd22dc1e4ea1481
s0172067 <113241344+s0172067@users.noreply.github.com>
2022-12-06 09:53:03 +0300

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -1,7 +1,7 @@
 let q;
 let f1 = document.getElementById("field2");
 let R = document.getElementById("result");
-let A=0;
+let A = 0;
 let B;
 let C;
 let i = 0;
@@ -18,17 +18,16 @@ window.addEventListener('DOMContentLoaded', function (event) {
     cheks.style.display = "none";
     s[0].addEventListener("change", function (event) {
         let select = event.target;
-        
+
         console.log(select.value);
-        // Можно использовать getElementsByClassName()
-        if (select.value == "300" || select.value=="1000") {
+        if (select.value == "300" || select.value == "1000") {
             radios.style.display = "none";
         }
         else {
             radios.style.display = "block";
         }
         if (select.value == "2000" || select.value == "1000") {
-           
+
             cheks.style.display = "none";
 
         }
@@ -36,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
             cheks.style.display = "block";
         }
         if (select.value == "300") {
-            i=3;
+            i = 3;
         }
         if (select.value == "2000") {
             i = 2;
@@ -49,73 +48,73 @@ window.addEventListener('DOMContentLoaded', function (event) {
 
 
 
-let r = document.getElementsByName("R");
-    
-    if (i == 1) {
-        A = 0;
-       // R.textContent = A;
-        
-    }
+        let r = document.getElementsByName("R");
+
+        if (i == 1) {
+            A = 0;
+            // R.textContent = A;
+
+        }
 
         if (i == 2) {
             A = 0;
-           
-        r[0].addEventListener("change", function (event) {
-            let select2 = event.target;
-            A = Number(select2.value);
-           // R.textContent = select2.value;
-        });
-    }
-    if (i == 3) {
-        C = 0;
+
+            r[0].addEventListener("change", function (event) {
+                let select2 = event.target;
+                A = Number(select2.value);
+                // R.textContent = select2.value;
+            });
+        }
+        if (i == 3) {
+            C = 0;
 
 
 
 
-        c[0].addEventListener("change", function (event) {
-            let select2 = event.target;
+            c[0].addEventListener("change", function (event) {
+                let select2 = event.target;
 
-            if (select2.checked) {
-                a1 = Number(select2.value);
-            }
-            else {
-                
-                a1 = -1* Number(select2.value);
-            }
-            C = C + a1;
-            if (C < 0) {
-                C = 0;
-            }
-            A = C;
-            //R.textContent = A; 
-            
-            
-        });
-        C=0;
+                if (select2.checked) {
+                    a1 = Number(select2.value);
+                }
+                else {
 
-    }
+                    a1 = -1 * Number(select2.value);
+                }
+                C = C + a1;
+                if (C < 0) {
+                    C = 0;
+                }
+                A = C;
+                //R.textContent = A; 
+
+
+            });
+            C = 0;
+
+        }
         //R.textContent = "";
-        
 
-        
-        
+
+
+
     });
 
- 
+
 });
 
 
 function click1() {
-    
+
     let F1 = Math.abs(Number(f1.value));
-  
+
     if (F1 % 10 >= "0" && F1 % 10 <= "9") {
         if (f1.value >= 0) {
-            R.textContent = Number(f1.value) * (B+A);
+            R.textContent = Number(f1.value) * (B + A);
         }
         else R.textContent = " Not right(< 0 )!!! ";
     } else R.textContent = "Not numbers !!! ";
-   /* R.textContent = A;*/
+     //R.textContent = B;
 }
 let buttom = document.getElementById("button1");
 
